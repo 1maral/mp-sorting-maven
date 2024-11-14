bbbbc491b91a5b315f36d47adef046fb3d22c7c4
1maral <erdenemaral>
2024-11-14 14:00:41 -0600

    tests

--- src/test/java/edu/grinnell/csc207/sorting/TestSorter.java
+++ src/test/java/edu/grinnell/csc207/sorting/TestSorter.java
@@ -1,16 +1,12 @@
 package edu.grinnell.csc207.sorting;
 
-import edu.grinnell.csc207.util.ArrayUtils;
-
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+
+import edu.grinnell.csc207.util.ArrayUtils;
 
 /**
  * Tests of Sorter objects. Please do not use this class directly.
@@ -120,4 +116,74 @@ public class TestSorter {
     ArrayUtils.permute(original);
     assertSorts(expected, original, intSorter);
   } // permutedIntegers
+
+  /**
+   * Ensure that an array that is empty gets sorted correctly.
+   */
+  @Test
+  public void emptyArrTest() {
+    if (null == stringSorter) {
+      return;
+    } // if
+    String[] original = {};
+    String[] expected = {};
+    assertSorts(expected, original, stringSorter);
+  } // emptyArrTest
+
+  /**
+   * Ensure that a large array gets sorted correctly.
+   */
+  @Test
+  public void largeArrTest() {
+    int SIZE = 50000; 
+    if (null == intSorter) { 
+      return; 
+    } // if
+    Integer[] original = new Integer[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+      original[i] = i;
+    } // for
+    Integer[] expected = original.clone();
+    ArrayUtils.permute(original);
+    assertSorts(expected, original, intSorter);
+  } // largeArrTest
+
+  /**
+   * Ensure that a small array with negative int gets sorted correctly.
+   */
+  @Test
+  public void numArrTest() {
+    if (null == intSorter) {
+      return;
+    } // if
+    Integer[] original = { 1, -5, -10, 10, 100, 0, -9, -99, 11};
+    Integer[] expected = { -99, -10, -9, -5, 0, 1, 10, 11, 100};
+    assertSorts(expected, original, intSorter);
+  } // largeArrTest
+
+  /**
+   * Ensure that a small array with duplicate int gets sorted correctly.
+   */
+  @Test
+  public void dupArrTest() {
+    if (null == intSorter) {
+      return;
+    } // if
+    Integer[] original = { 1, 2, 1, 2, 1, 0, 3, 1, 3, 2};
+    Integer[] expected = { 0, 1, 1, 1, 1, 2, 2, 2, 3, 3};
+    assertSorts(expected, original, intSorter);
+  } // largeArrTest
+
+  /**
+   * Ensure that a string array gets sorted correctly.
+   */
+  @Test
+  public void stringTest() {
+    if (null == stringSorter) {
+      return;
+    } // if
+    String[] original = {"maral1", "maral2", "maral bat", "maral bate", "1maral", "2maral"};
+    String[] expected = {"1maral", "2maral", "maral1", "maral2", "maral bat", "maral bate"};
+    assertSorts(expected, original, stringSorter);
+  } // emptyArrTest
 } // class TestSorter
